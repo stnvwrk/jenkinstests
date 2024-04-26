a84c630682b3179e7a5eebbcc2c6ae9624b5c0c4
Daniel Stoynov <daniel.stoynov@egt-digital.com>
2024-04-26 11:39:42 +0300

    dev loop

--- mybashscript.sh
+++ mybashscript.sh
@@ -3,5 +3,17 @@
 echo "Hello $1 I'm working!"
 
 
+sed '/<child>/,/<\\/child/d' testfile.xml > edited_testfile.xml
+
+values="10 22 23 33 45 66 78"
+
+for value in ${values[@]}
+do
+  value_tag="<child>\n<value>$value</value>\n</child>\n"
+  value_tags="${value_tags} ${value_tag}"
+done
+
+sed "/<parent>/a ${values_tags}" edited_testfile.xml > final_testfile_${1}.xml
+cat final_testfile_${1}.xml
 
 
